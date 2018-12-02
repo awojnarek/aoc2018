5efd3e93da46b3e76a2d45396a681f4460215a81
Andy Wojnarek <andy.wojnarek@theatsgroup.com>
2018-12-01 19:54:42 -0500

    fixing bullshit

--- 1/main.go
+++ 1/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
@@ -21,17 +20,8 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		input := scanner.Text()
-		stringOperator := strings.Split(string(input), "")[0]
-		stringNumber := input[1:]
-		integer, _ := strconv.Atoi(stringNumber)
-
-		if stringOperator == "+" {
-			count = count + integer
-		}
-
-		if stringOperator == "-" {
-			count = count - integer
-		}
+		integer, _ := strconv.Atoi(input)
+		count = count + integer
 	}
 	fmt.Println(count)
 }
