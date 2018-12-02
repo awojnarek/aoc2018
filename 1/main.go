315c0bfb9800cd6e426779e9e6f549f928ec1a6b
Andy Wojnarek <andy.wojnarek@theatsgroup.com>
2018-12-02 15:32:49 -0500

    fucked

--- 1/main.go
+++ 1/main.go
@@ -17,11 +17,27 @@ func main() {
 	defer file.Close()
 
 	count := 0
+	m := make(map[int]int)
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		// Convert to integer
 		input := scanner.Text()
 		integer, _ := strconv.Atoi(input)
+
+		// Add to our existing count
 		count = count + integer
+
+		// Check to see if this new count is already in our map
+		_, present := m[count]
+
+		// If it's in our map already, break
+		if present == true {
+			fmt.Println(count)
+			os.Exit(0)
+		}
+
+		// It's not - add it to our map
+		m[count] = 1
 	}
-	fmt.Println(count)
 }
